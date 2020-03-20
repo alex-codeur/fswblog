943d9b931cf1d773fc492c0351925f3dc69047f6
Alex Esaki <apachecordovax@gmail.com>
2020-03-20 10:43:58 +0100

    mise en place des relations polymorphiques

--- app/Post.php
+++ app/Post.php
@@ -4,7 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\User;
+use App\Category;
+
 class Post extends Model
 {
-    //
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+//Posts
+Route::get('post', 'PostController@all_post');
