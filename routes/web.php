17185bc57aa8332bb6e8df12490cc3d66a6adfd5
Alex Esaki <apachecordovax@gmail.com>
2020-03-24 06:07:16 +0100

    é

--- routes/web.php
+++ routes/web.php
@@ -20,17 +20,19 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 //Route::get('/{anypath}', 'HomeController@index')->where('path','.*');
+Route::group(['middleware' => ['auth']], function() {
+    // Category
+    Route::get('/category', 'CategoryController@all_category');
+    Route::post('/add-category', 'CategoryController@add_category');
+    Route::get('/editCategory/{id}', 'CategoryController@edit_category');
+    Route::patch('/update-category/{id}', 'CategoryController@update_category');
+    Route::delete('/category/{id}', 'CategoryController@delete_category');
 
-// Category
-Route::get('/category', 'CategoryController@all_category');
-Route::post('/add-category', 'CategoryController@add_category');
-Route::get('/editCategory/{id}', 'CategoryController@edit_category');
-Route::patch('/update-category/{id}', 'CategoryController@update_category');
-Route::delete('/category/{id}', 'CategoryController@delete_category');
+    // Post
+    Route::get('/post', 'PostController@all_post');
+    Route::post('/savepost', 'PostController@save_post');
+    Route::get('/post/{id}', 'PostController@edit_post');
+    Route::patch('/update/{id}', 'PostController@update_post');
+    Route::delete('/delete/{id}', 'PostController@delete_post');
+});
 
-// Post
-Route::get('/post', 'PostController@all_post');
-Route::post('/savepost', 'PostController@save_post');
-Route::get('/post/{id}', 'PostController@edit_post');
-Route::patch('/update/{id}', 'PostController@update_post');
-Route::delete('/delete/{id}', 'PostController@delete_post');
